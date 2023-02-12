4eaba82a990eb42b00cd67f30931e221db41ebd8
Jazz Parayaoan <jparayaoan19@mail.wou.edu>
2023-02-11 23:37:54 -0800

    added base project and created empty backstory view and controller

---
+++ src/DWC_NightOwlProject/DWC_NightOwlProject/Controllers/BackstoryController.cs
@@ -0,0 +1,14 @@
+﻿using System.Diagnostics;
+using Microsoft.AspNetCore.Mvc;
+using DWC_NightOwlProject.Models;
+
+namespace DWC_NightOwlProject.Controllers
+{
+    public class BackstoryController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
