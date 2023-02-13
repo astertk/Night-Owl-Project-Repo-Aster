bb4b70dd2a7e92b7750c00fca77b55aa873e77e2
Jazz Parayaoan <jparayaoan19@mail.wou.edu>
2023-02-13 10:24:25 -0800

    deleted empty controller and view and added a new controller with CRUD operations and view still empty

--- src/DWC_NightOwlProject/DWC_NightOwlProject/Controllers/BackstoryController.cs
+++ src/DWC_NightOwlProject/DWC_NightOwlProject/Controllers/BackstoryController.cs
@@ -1,17 +1,86 @@
-﻿using System.Diagnostics;
-using Microsoft.AspNetCore.Mvc;
-using OpenAI_API;
+﻿using OpenAI_API;
 using OpenAI_API.Models;
 using OpenAI_API.Files;
-using DWC_NightOwlProject.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
 
 namespace DWC_NightOwlProject.Controllers
 {
     public class BackstoryController : Controller
     {
-        public IActionResult Index()
+        // GET: HomeController1
+        public ActionResult Index()
         {
             return View();
+        }
+
+        // GET: HomeController1/Details/5
+        public ActionResult Details(int id)
+        {
+            return View();
+        }
+
+        // GET: HomeController1/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: HomeController1/Create
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create(IFormCollection collection)
+        {
+            try
+            {
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: HomeController1/Edit/5
+        public ActionResult Edit(int id)
+        {
+            return View();
+        }
+
+        // POST: HomeController1/Edit/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit(int id, IFormCollection collection)
+        {
+            try
+            {
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: HomeController1/Delete/5
+        public ActionResult Delete(int id)
+        {
+            return View();
+        }
+
+        // POST: HomeController1/Delete/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Delete(int id, IFormCollection collection)
+        {
+            try
+            {
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
         }
     }
 }
