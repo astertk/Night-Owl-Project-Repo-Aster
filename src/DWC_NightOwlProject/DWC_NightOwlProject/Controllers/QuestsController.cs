3d428fa8d3a3efcb13a0ec6855582ba97111517e
Aster Kubo <akubo14@mail.wou.edu>
2023-03-06 21:26:59 -0800

    implemented creating quest

--- src/DWC_NightOwlProject/DWC_NightOwlProject/Controllers/QuestsController.cs
+++ src/DWC_NightOwlProject/DWC_NightOwlProject/Controllers/QuestsController.cs
@@ -2,6 +2,12 @@
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Identity;
 using DWC_NightOwlProject.DAL.Abstract;
+using DWC_NightOwlProject.ViewModel;
+using DWC_NightOwlProject.Data;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.EntityFrameworkCore;
+using OpenAI;
 
 namespace DWC_NightOwlProject.Controllers
 {
@@ -10,13 +16,15 @@ namespace DWC_NightOwlProject.Controllers
         private readonly IConfiguration _config;
         private readonly UserManager<IdentityUser> _userManager;
         private IMaterialRepository _materialRepository;
+        private IWorldRepository worldRepo;
         private readonly string materialType="Quest";
 
-        public QuestsController(IConfiguration config, UserManager<IdentityUser> um, IMaterialRepository mrepo)
+        public QuestsController(IConfiguration config, UserManager<IdentityUser> um, IMaterialRepository mrepo,IWorldRepository wRepo)
         {
             _config=config;
             _userManager=um;
             _materialRepository=mrepo;
+            worldRepo=wRepo;
         }
 
         // GET: QuestController
@@ -31,12 +39,95 @@ namespace DWC_NightOwlProject.Controllers
             return View();
         }
 
+        [Authorize]
         public ActionResult GuidedQuestTemplate()
         {
             return View();
         }
 
+        public ActionResult ContinueGuidedQuest()
+        {
+            return View();
+        }
+
+        [Authorize]
+        [HttpPost]
+        public async Task<ActionResult> ContinueGuidedQuest(GuidedQuestViewModel qm)
+        {
+            GuidedQuestViewModel prevModel;
+            var key=_config["APIKey"];
+            var api=new OpenAIClient(new OpenAIAuthentication(key));
+            if(qm.CurrentStep==null)
+            {
+                ViewBag.Message= "Must provide step input.";
+                return View(qm);
+            }
+            else
+            {
+                var result = await api.CompletionsEndpoint.CreateCompletionAsync(qm.LatestStep(), max_tokens: 1000, temperature: .5, presencePenalty: .5, frequencyPenalty: .5, model: OpenAI.Models.Model.Davinci);
+                qm.AddResult(result.ToString());
+                return View(qm);
+            }
+        }
+
+        [HttpPost]
+        public IActionResult SaveGuidedQuest(GuidedQuestViewModel qm)
+        {
+            String userId = _userManager.GetUserId(User);
+            World userWorld=getUserWorld(userId);
+            Material m= new Material();
+            m.Type=materialType;
+            m.UserId=userId;
+            m.WorldId=userWorld.Id;
+            m.Completion=qm.Results;
+            if(userId!=null)
+            {
+                try
+                {
+                    _materialRepository.AddOrUpdate(m);
+                    ViewBag.Message("Quest saved");
+                    return View("ContinueGuidedQuest", qm);
+                }
+                catch(DbUpdateConcurrencyException e)
+                {
+                    ViewBag.Message = "An unknown database error occurred while trying to create the item.  Please try again.";
+                    return View("ContinueGuidedQuest", qm);
+                }
+            }
+            return View("ContinueGuidedQuest", qm);
+        }
+
+/**        [HttpPost]
+       public async Task<ActionResult> StartGuidedQuest(GuidedQuestViewModel quest)
+        {
+            var key=_config["APIKey"];
+            var api=new OpenAIClient(new OpenAIAuthentication(key));
+            ContinueGuidedQuestViewModel qm=new ContinueGuidedQuestViewModel();
+            qm.QuestDetails=quest;
+            bool success=qm.QuestDetails.AddStep();
+            if(!success)
+            {
+                ViewBag.Message= "Must provide step input. Cannot have more than " +GuidedQuestViewModel.stepMax+" steps.";
+                return View("ContinueGuidedQuest",qm);
+            }
+            else
+            {
+                var result = await api.CompletionsEndpoint.CreateCompletionAsync(qm.QuestDetails.LatestStep(), max_tokens: 1000, temperature: .5, presencePenalty: .5, frequencyPenalty: .5, model: OpenAI.Models.Model.Davinci);
+                qm.QuestDetails.AddResult(result.ToString());
+                return View("ContinueGuidedQuest",qm);
+            }
+        }**/
+
         // GET: QuestController/Create
+        public World getUserWorld(string userid)
+        {
+            World w=worldRepo.GetUserWorld(userid);
+            if(w!=null)
+            {
+                return w;
+            }
+            return null;
+        }
         public ActionResult Create()
         {
             return View();
