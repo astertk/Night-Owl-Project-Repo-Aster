d04b0fe2a910606aec16e94fddabe99cc17d9e74
unknown <oneilmagno@gmail.com>
2023-03-19 16:49:41 -0700

    edited authmessengeroptions to enable azure work

--- src/DWC_NightOwlProject/DWC_NightOwlProject/Services/AuthMessageSenderOptions.cs
+++ src/DWC_NightOwlProject/DWC_NightOwlProject/Services/AuthMessageSenderOptions.cs
@@ -4,5 +4,10 @@
     {
         //public string SendGridUser { get; set; }
         public string? SendGridKey { get; set; }
+        public AuthMessageSenderOptions()
+        {
+            // Set the SendGrid API key
+            //SendGridKey = "YOUR_API_KEY_HERE";
+        }
     }
 }
