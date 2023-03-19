4d7cfbf017dc20edaf2828634bbb4d8cfae29918
unknown <oneilmagno@gmail.com>
2023-03-19 13:34:08 -0700

    Added confirming email user story

---
+++ src/DWC_NightOwlProject/DWC_NightOwlProject/Services/AuthMessageSenderOptions.cs
@@ -0,0 +1,8 @@
+﻿namespace DWC_NightOwlProject.Services
+{
+    public class AuthMessageSenderOptions
+    {
+        //public string SendGridUser { get; set; }
+        public string? SendGridKey { get; set; }
+    }
+}
