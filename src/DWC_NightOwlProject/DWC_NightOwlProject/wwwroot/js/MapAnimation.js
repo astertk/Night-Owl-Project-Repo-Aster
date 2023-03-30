09a16e0ba169b2eb0c33f1ca03b86dc590aa1503
Jleemcmichael97 <jleemcmichael18@mail.wou.edu>
2023-03-30 15:18:43 -0700

    Got the pictures to preload properly on map index

--- src/DWC_NightOwlProject/DWC_NightOwlProject/wwwroot/js/MapAnimation.js
+++ src/DWC_NightOwlProject/DWC_NightOwlProject/wwwroot/js/MapAnimation.js
@@ -4,21 +4,26 @@ let rightX;
 let rightY;
 
 function preload() {
-    left = loadImage('left-shoe-footprint.png');
-    right = loadImage('right-shoe-footprint.png');
+    left = loadImage('css/left-shoe-footprint.png');
+    right = loadImage('css/right-shoe-footprint.png');
 }
+//
 
 function setup() {
     background(255);
     createCanvas(windowWidth, windowHeight);
     angleMode(DEGREES);
-    leftX = 0;
+    leftX = 1;
     rightX = 0;
     leftY = height / 2;
     rightY = height / 2;
 }
 
 function draw() {
+
+    //document.getElementById("Create").addEventListener("click", alert("this is working"));
+
+
     rightRate = frameCount + 60;
     leftRate = frameCount;
 
@@ -41,3 +46,4 @@ function draw() {
 
 
 }
+
