eecc479fdc1e87c528519621ff735f01e5008002
Jleemcmichael97 <jleemcmichael18@mail.wou.edu>
2023-03-31 10:36:33 -0700

    added function for divs to appear and disappear on click

--- src/DWC_NightOwlProject/DWC_NightOwlProject/wwwroot/js/site.js
+++ src/DWC_NightOwlProject/DWC_NightOwlProject/wwwroot/js/site.js
@@ -1,2 +1,27 @@
-﻿
+﻿var rToggle = 0;
 
+
+function reveal(id, id2) {
+
+    
+
+    var myDiv = document.getElementById(id);
+
+
+
+    if (rToggle == 0) {
+
+        myDiv.style.display = 'block';
+        rToggle = 1;
+    }
+
+    else {
+        myDiv.style.display = 'none'
+        rToggle = 0;
+    }
+
+    var hide = document.getElementById(id2);
+
+    hide.style.display = 'none';
+
+}
