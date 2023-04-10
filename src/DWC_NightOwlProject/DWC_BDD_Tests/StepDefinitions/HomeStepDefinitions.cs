fb5b552e712c17bbdb84f27deb525770a2dd6f68
Jleemcmichael97 <jleemcmichael18@mail.wou.edu>
2023-04-09 21:42:25 -0700

    Got the test implemented, not yet passing

---
+++ src/DWC_NightOwlProject/DWC_BDD_Tests/StepDefinitions/HomeStepDefinitions.cs
@@ -0,0 +1,39 @@
+using DWC_BDD_Tests.Drivers;
+using System;
+using TechTalk.SpecFlow;
+using NUnit.Framework;
+using DWC_BDD_Tests.Drivers;
+using DWC_BDD_Tests.PageObjects;
+using System;
+using TechTalk.SpecFlow;
+
+namespace DWC_BDD_Tests.StepDefinitions
+{
+    [Binding]
+    public class HomeStepDefinitions
+    {
+        private readonly HomePageObject _homePage;
+        public HomeStepDefinitions(BrowserDriver browserDriver)
+        {
+
+        }
+
+        [Given(@"I am a visitor")]
+        public void GivenIAmAVisitor()
+        {
+            // nothing to do!!
+        }
+
+        [When(@"I am on the ""([^""]*)"" page")]
+        public void WhenIAmOnThePage(string pageName)
+        {
+            _homePage.GoTo(pageName);
+        }
+
+        [Then(@"The page footer contains ""([^""]*)""")]
+        public void ThenThePageFooterContains(string p0)
+        {
+            _homePage.GetLogo().ToString().Contains(p0);
+        }
+    }
+}
