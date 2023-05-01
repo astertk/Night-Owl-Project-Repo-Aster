cedd69fd5afe86389959b7e642e6234ca8c05b64
Jleemcmichael97 <jleemcmichael18@mail.wou.edu>
2023-04-30 23:14:53 -0700

    fixed up maps template, working on maps animation.

--- src/DWC_NightOwlProject/DWC_NightOwlProject/wwwroot/js/MapAnimation.js
+++ src/DWC_NightOwlProject/DWC_NightOwlProject/wwwroot/js/MapAnimation.js
@@ -1,67 +1,90 @@
-﻿let leftX;
-let leftY;
-let rightX;
-let rightY;
+﻿let imgHex;
+let angle = 0;
+let nums = ["1","2","3","4","5","6","7","8","9","10","11","12","13","14","15","16","17","18","19","20"]
+let it = 0;
 let drawToggle = 0;
 
-function preload() {
-    left = loadImage('css/left-shoe-footprint.png');
-    right = loadImage('css/right-shoe-footprint.png');
+function preload(){
+    imgHex = loadImage('/css/icosahedron.png')
 }
-//
+
+let x = 0;
+let y = 0;
 
 function setup() {
-    background(255);
-    createCanvas(windowWidth, windowHeight);
-    angleMode(DEGREES);
-    leftX = width/7;
-    rightX = width / 7 - 1;
-    leftY = height / 5;
-    rightY = height / 5;
-    
+  w = min(windowWidth, windowHeight);
+  createCanvas(windowWidth, windowHeight);
+
+  // gridWidth = w;
+  // gridHeight = w;
+  // hexagonSize = w / 10;
+  // hexNum = 50;
+  angleMode(DEGREES);
+  
 }
+
+function drawHexagon(cX, cY, r) {
+  beginShape();
+
+  for (let a = 0; a < TAU; a += TAU / 6) {
+    vertex(cX + r * cos(a), cY + r * sin(a));
+  }
+  
+  endShape(CLOSE);
+}
+
+
+
 
 function draw() {
-
-    
-
-    if (drawToggle == 1) { 
-    
-  
-
-
-    rightRate = frameCount + 60;
-    leftRate = frameCount;
-
-
-
-    image(left, leftX + 50, leftY - 50, 100, 100);
-    image(right, rightX, rightY, 100, 100);
-
-
-    if (leftRate % 120 == 0) {
-        leftX += 150;
-
-    }
-
-    if (rightRate % 120 == 0) {
-        rightX += 150;
-
+    if (drawToggle < 1) {
+        frameCount = 0;
     }
 
 
+    if (drawToggle == 1) {
+        let numOutcome = random(0, 19);
+        
+
+
+        background(255);
+        imageMode(CENTER);
+
+        fill(it / 10, 0, 0);
+        rect(0, height / 2 - 20, it - 50, 50, 20);
+
+
+        // nums = frameCount.ToString();
+
+        // Red square at (100, 100)
+        push();
+        translate(frameCount, height / 2);
+        rotate(angle);
+        rotate(angle);
+        rotate(angle);
+
+        image(imgHex, 0, 0, 100, 100);
+        push();
+        text(nums[19], -7, 5);
+        frameRate(1);
+        pop();
+        frameRate(60);
+
+
+        pop();  //The origin is back to (0, 0) and rotation is back to 0.
+        angle += 1;
+
+
+        // drawHexagon(frameCount,height/2,hexagonSize);
+
+
+
+        x += 0.05; //this is the speed of x
+        y += 0.04; // this is the speed of y
+        it++;
     }
 }
-
 
 function toggle() {
     drawToggle = 1;
 }
-
-
-
-
-
-
-
-
--- src/DWC_NightOwlProject/DWC_NightOwlProject/wwwroot/js/site.js
+++ src/DWC_NightOwlProject/DWC_NightOwlProject/wwwroot/js/site.js
@@ -30,29 +30,3 @@ function reveal(id, id2) {
 }
 
 
-function uploading() {
-}
-
-
-
-/*
-
-document.forms.edit_image_form.addEventListener('submit', e => {
-        e.preventDefault();
-
-        let png = ['<?xml version="1.0"?><png xmlns="http://www.w3.org/2000/png" width="200" height="200"><rect fill="blue" width="200" height="200" x="0" y="0"/></png>'];
-        let file = new File(png, 'example.png', { type: 'image/png' });
-
-        let formData = new FormData(e.target);
-        formData.set('file', file, 'example.png');
-
-        fetch('/', {
-            method: 'POST',
-            body: formData
-        })
-            .then(response => response.json())
-            .then(data => console.log(data));
-    });*/
-
-
-
