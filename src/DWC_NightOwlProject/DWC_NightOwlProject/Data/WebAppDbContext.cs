7a07e54ba3adba3adccca7de0a5aca42af4df98d
unknown <oneilmagno@gmail.com>
2023-05-21 01:10:12 -0700

    Fixed random character generation and bug fix with delete

--- src/DWC_NightOwlProject/DWC_NightOwlProject/Data/WebAppDbContext.cs
+++ src/DWC_NightOwlProject/DWC_NightOwlProject/Data/WebAppDbContext.cs
@@ -38,44 +38,42 @@ public partial class WebAppDbContext : DbContext
     {
         modelBuilder.Entity<Backstory>(entity =>
         {
-            entity.HasKey(e => e.Id).HasName("PK__Backstor__3214EC2746A7337D");
+            entity.HasKey(e => e.Id).HasName("PK__Backstor__3214EC272EEC4C15");
         });
 
         modelBuilder.Entity<Character>(entity =>
         {
-            entity.HasKey(e => e.Id).HasName("PK__Characte__3214EC27CBE8BE20");
+            entity.HasKey(e => e.Id).HasName("PK__Characte__3214EC278C06DEAE");
         });
 
         modelBuilder.Entity<Encounter>(entity =>
         {
-            entity.HasKey(e => e.Id).HasName("PK__Encounte__3214EC27624B2244");
+            entity.HasKey(e => e.Id).HasName("PK__Encounte__3214EC2710161A2B");
         });
 
         modelBuilder.Entity<Map>(entity =>
         {
-            entity.HasKey(e => e.Id).HasName("PK__Maps__3214EC279AD246EF");
+            entity.HasKey(e => e.Id).HasName("PK__Maps__3214EC27443DE23B");
         });
 
         modelBuilder.Entity<Material>(entity =>
         {
-            entity.HasKey(e => e.Id).HasName("PK__Material__3214EC2750B3F0D1");
+            entity.HasKey(e => e.Id).HasName("PK__Material__3214EC27D0256839");
         });
 
         modelBuilder.Entity<Quest>(entity =>
         {
-            entity.HasKey(e => e.Id).HasName("PK__Quests__3214EC27EC70E163");
-            entity.HasKey(e => e.Id).HasName("PK__Quests__3214EC2705807337");
-            entity.HasKey(e => e.Id).HasName("PK__Quests__3214EC27C9724F1A");
+            entity.HasKey(e => e.Id).HasName("PK__Quests__3214EC2780B9D691");
         });
 
         modelBuilder.Entity<Song>(entity =>
         {
-            entity.HasKey(e => e.Id).HasName("PK__Songs__3214EC2711E89ACD");
+            entity.HasKey(e => e.Id).HasName("PK__Songs__3214EC27B62E85F7");
         });
 
         modelBuilder.Entity<World>(entity =>
         {
-            entity.HasKey(e => e.Id).HasName("PK__World__3214EC27965AB62E");
+            entity.HasKey(e => e.Id).HasName("PK__World__3214EC27268170AA");
         });
 
         OnModelCreatingPartial(modelBuilder);
